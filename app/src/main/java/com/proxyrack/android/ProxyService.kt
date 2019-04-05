ac214777b2f5f5ab6be5d24a32b4092aa46a315f
Alex Sheiko <alexsheikodev@gmail.com>
2019-04-06 01:17:57 +0300

    Read TLS response from example server

--- app/src/main/java/com/proxyrack/android/ProxyService.kt
+++ app/src/main/java/com/proxyrack/android/ProxyService.kt
@@ -14,24 +14,18 @@ import com.proxyrack.network.service.GeolocationService
 import io.reactivex.Flowable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.android.schedulers.AndroidSchedulers.mainThread
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import io.reactivex.schedulers.Schedulers.io
-import java.io.BufferedReader
-import java.io.DataOutputStream
-import java.io.InputStreamReader
-import java.io.PrintWriter
+import java.io.*
 import java.util.concurrent.TimeUnit
 import javax.net.ssl.SSLSocket
 import javax.net.ssl.SSLSocketFactory
 
-
 class ProxyService : Service() {
 
     private var socket: SSLSocket? = null
-    private var readerStream: BufferedReader? = null
-    private var writerStream: DataOutputStream? = null
+    private var readerStream: InputStream? = null
+    private var writerStream: OutputStream? = null
     private val disposables = CompositeDisposable()
 
     override fun onBind(intent: Intent): IBinder? {
@@ -42,29 +36,69 @@ class ProxyService : Service() {
 
         disposables.add(
             getLocation()
-            .map {
-                val message = MessageToServer("hello", getDeviceId(), it.city, it.country)
-                connectToServer(message)
-            }
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                Log.d(ProxyService::class.java.simpleName, "Finished")
-            }, { t ->
-                Toast.makeText(baseContext, t.localizedMessage, Toast.LENGTH_LONG).show()
-            })
+                .map {
+                    val message = MessageToServer("hello", getDeviceId(), it.city, it.country)
+                    connectToServer(message)
+                }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    Log.d(ProxyService::class.java.simpleName, "Finished")
+                }, { t ->
+                    Toast.makeText(baseContext, t.localizedMessage, Toast.LENGTH_LONG).show()
+                })
         )
     }
 
-    override fun onDestroy() {
-        // Cleanup
-        readerStream?.close()
-        writerStream?.close()
+    /**
+     * Connects to backconnect server via SSL
+     */
+    private fun connectToServer(message: MessageToServer) {
 
-        if (socket != null) {
-            socket!!.close()
-        }
-        disposables.clear()
+        val host = "monetizemyapp.net"
+        val port = 8080 // default secure port
+
+        // Create and open a connection
+        socket = createSocket(host, port)
+
+        // Prepare json body, indicating end of string with a special char
+        val json = Gson().toJson(message) + "\\0"
+
+        writerStream = BufferedOutputStream(socket!!.outputStream)
+        readerStream = BufferedInputStream(socket!!.inputStream)
+
+        // Send hello message to server
+        writerStream!!.write(json.toByteArray())
+        writerStream!!.flush()
+
+        // Server won't respond to hello message,
+        // but we need to start listening for user-initiated connection requests
+        disposables.add(
+            Flowable.interval(1, TimeUnit.SECONDS)
+                .map {
+                    // Check for response from server
+                    val data = ByteArray(2048)
+                    val len = readerStream!!.read(data)
+                    if (len > 0) {
+                        val response = String(data, 0, len)
+                        Log.d(ProxyService::class.java.simpleName, response)
+                    } else {
+                        Log.d(ProxyService::class.java.simpleName, "No data received")
+                    }
+                }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ }, { e -> throw e })
+        )
+    }
+
+    @Throws(IOException::class)
+    fun createSocket(host: String, port: Int): SSLSocket {
+
+        val socket = SSLSocketFactory.getDefault()
+            .createSocket(host, port) as SSLSocket
+        socket.enabledProtocols = arrayOf("TLSv1.2")
+        return socket
     }
 
     /**
@@ -76,44 +110,14 @@ class ProxyService : Service() {
         return geoService.location
     }
 
-    /**
-     * Connects to backconnect server via SSL
-     */
-    private fun connectToServer(message: MessageToServer) {
+    override fun onDestroy() {
+        // Cleanup
+        readerStream?.close()
+        writerStream?.close()
 
-        val host = "68.183.109.83"
-        val port = 443 // default secure port
-
-        // Create and open a connection
-        val socketFactory = SSLSocketFactory.getDefault() as SSLSocketFactory
-        socket = socketFactory.createSocket(host, port) as SSLSocket
-
-        // Prepare json body, indicating end of string with a special char
-        val json = Gson().toJson(message) + "\\0"
-
-        writerStream = DataOutputStream(socket!!.outputStream)
-        readerStream = BufferedReader(InputStreamReader(socket!!.inputStream))
-
-        // Send json message to server
-        val writer = PrintWriter(writerStream!!)
-        writer.println(json)
-        writer.flush()
-
-        // Server won't respond to hello message,
-        // but we need to start listening for user-initiated connection requests
-        disposables.add(
-            Flowable.interval(1, TimeUnit.SECONDS)
-                .subscribeOn(io())
-                .observeOn(mainThread())
-                .subscribe({
-
-                    var line: String
-                    while (readerStream!!.ready()) {
-                        line = readerStream!!.readLine()
-                        Log.d(ProxyService::class.java.simpleName, "Backconnect message: $line")
-                    }
-
-                }, { e -> throw e })
-        )
+        if (socket != null) {
+            socket!!.close()
+        }
+        disposables.clear()
     }
 }
